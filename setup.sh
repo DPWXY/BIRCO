6f3c78cd1f634b983d55cbcedcd0703cd3b158b1
BIRCO-benchmark <bircobirco.benchmark@gmail.com>
2023-12-29 22:23:15 +0800

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch-nightly -c nvidia
+pip install beir
+pip install tokenizers==0.12.1
+pip install -U adapters
+
+pip install transformers
+pip install openai==0.27.8
+pip install huggingface_hub==0.20.1
+pip install --upgrade huggingface_hub
